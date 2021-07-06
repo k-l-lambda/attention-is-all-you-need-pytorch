9e34ae9b622f2c8e3e02107e87bb1bc5067e8e92
k.l.lambda <k.l.lambda@gmail.com>
2021-07-06 14:55:24 +0800

    preprocess_single.py created.

---
+++ preprocess_single.py
@@ -0,0 +1,55 @@
+
+import io
+import logging
+import dill as pickle
+from collections import Counter
+import argparse
+import torchtext
+import torch
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import Vocab
+
+
+
+def buildFromFile (file_path, tokenizer_type = None):
+	tokenizer = get_tokenizer(tokenizer_type)
+
+	# build vocab
+	counter = Counter()
+	with io.open(file_path, 'rt') as f:
+		for line in f:
+			counter.update(tokenizer(line))
+	vocab = Vocab(counter, specials=['<unk>', '<pad>', '<bos>', '<eos>'])
+	#print('vocab:', vocab)
+
+	examples = []
+	raw_iter = iter(io.open(file_path, 'rt'))
+	for raw in raw_iter:
+		tensor = torch.tensor([vocab[token] for token in tokenizer(raw)], dtype=torch.long)
+		examples.append(tensor)
+
+	return vocab, examples
+
+
+def main ():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('source', type=str, help='input text file')
+	parser.add_argument('target', type=str, help='output package file')
+	opt = parser.parse_args()
+
+	logging.info('Building package from source: %s', opt.source)
+	vocab, examples = buildFromFile(opt.source)
+	#print('examples:', examples)
+
+	data = {
+		'vocab': vocab,
+		'examples': examples,
+	}
+	pickle.dump(data, open(opt.target, 'wb'))
+
+	logging.info('Done.')
+
+
+
+if __name__ == '__main__':
+	main()
